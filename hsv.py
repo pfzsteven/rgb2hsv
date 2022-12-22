fbb875967d10d739fecab1d81b1617e9a47ab388
pf.z <zpfkufei@gmail.com>
2022-12-22 21:50:36 +0800

    Update hsv.py

--- hsv.py
+++ hsv.py
@@ -1,25 +1,43 @@
 import sys
 
 
-def rgb2hsv(r, g, b):
-    r, g, b = r / 255.0, g / 255.0, b / 255.0
-    mx = max(r, g, b)
-    mn = min(r, g, b)
-    df = mx - mn
-    if mx == mn:
-        h = 0
-    elif mx == r:
-        h = (60 * ((g - b) / df) + 360) % 360
-    elif mx == g:
-        h = (60 * ((b - r) / df) + 120) % 360
-    elif mx == b:
-        h = (60 * ((r - g) / df) + 240) % 360
-    if mx == 0:
-        s = 0
+def RGBTOHSVHelper(offset, dominantcolor, colorone, colortwo):
+    V = dominantcolor
+    if V != 0.0:
+        if colorone <= colortwo:
+            num1 = colorone
+        else:
+            num1 = colortwo
+            pass
+        num2 = V - num1
+        if num2 != 0.0:
+            S = num2 / V
+            H = offset + (colorone - colortwo) / num2
+            pass
+        else:
+            S = 0.0
+            H = offset + (colorone - colortwo)
+            pass
+        H = H / 6.0
+        if H >= 0.0:
+            return (H, S, V)
+        H = H + 1
+        pass
     else:
-        s = df / mx
-    v = mx
-    return h, s, v
+        S = 0
+        H = 0
+        pass
+    return (H, S, V)
+
+
+def rgb2hsv(r: float, g: float, b: float):
+    if b > g and b > r:
+        hsv = RGBTOHSVHelper(4.0, b, r, g)
+    elif g > r:
+        hsv = RGBTOHSVHelper(2.0, g, b, r)
+    else:
+        hsv = RGBTOHSVHelper(0.0, r, g, b)
+    return (int(hsv[0] * 360), int(hsv[1] * 100), int(hsv[2] * 100))
 
 
 def hex_to_rgb(hex):
@@ -32,11 +50,12 @@ def rgb_to_hex(r, g, b):
 
 def print_hsv(rgb_str):
     rgb_arr = hex_to_rgb(rgb_str)
-    hsv = rgb2hsv(rgb_arr[0], rgb_arr[1], rgb_arr[2])
-    h = '{:.2f}'.format(hsv[0])
-    s = '{:.2f}'.format(hsv[1])
-    v = '{:.2f}'.format(hsv[2])
-    print("%s:%s,%s,%s" % (rgb_str, h, s, v))
+    r = rgb_arr[0] / 255.0
+    g = rgb_arr[1] / 255.0
+    b = rgb_arr[2] / 255.0
+    # print("rgb is:", (r, g, b))
+    hsv = rgb2hsv(r, g, b)
+    print("%s:%s,%s,%s" % (rgb_str, hsv[0], hsv[1], hsv[2]))
 
 
 if __name__ == '__main__':
